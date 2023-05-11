0476ad684e3a8134549102b96eee4f32aabf81ce
aimanaisha <aelinhathway@gmail.com>
2023-05-11 22:34:30 +0530

    landing page and navbar

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+      <main className="font-audiowide">
+        <h1><span></span> KNOW ME</h1>
+        <div></div>
+      </main>
+    )
+  }
---
+++ src/app/page.tsx
@@ -0,0 +1,71 @@
+
+
+
+export default function Home() {
+
+
+
+  return (
+    <main className="">
+      
+      <div className="font-audiowide h-full justify-center ml-10 flex flex-col">
+        <span className="align-end text-[#35445A] font-caveat text-3xl">
+          &#60;div&#62;
+        </span>
+
+        <div className="leading-none flex flex-col w-fit ml-10">
+          <span className="align-end text-[#35445A] font-caveat text-3xl">
+            &#60;h2&#62;
+          </span>
+          <h1 className="text-[5rem] text-[#7C86A3] ml-10">HELLO!</h1>
+          <span className=" self-end text-[#35445A] font-caveat text-3xl relative left-10">
+            &#60;/h2&#62;
+          </span>
+        </div>
+
+        <div className="flex flex-col w-fit leading-[8rem] ml-10">
+          <span className="align-end text-[#35445A] font-caveat text-3xl">
+            &#60;h1&#62;
+          </span>
+          <h1 className="text-[10rem] text-[#B8B6B6] ml-10">
+            I AM{" "}
+            <span id="my-div" className="text-[#621AD7] h-20 w-20 font-monoton text-[9rem]">
+              AIMAN
+            </span>
+            .
+          </h1>
+          <span className="self-end text-[#35445A] font-caveat text-3xl relative left-10">
+            &#60;/h1&#62;
+          </span>
+        </div>
+
+        <div className="leading-none w-fit flex flex-col ml-10">
+          <span className="align-end text-[#35445A] font-caveat text-3xl">
+            &#60;h2&#62;
+          </span>
+          <h2 className="text-[6rem] text-[#B8B6B6] ml-10">
+            I LIKE TO CREATE
+          </h2>
+          <span className="self-end text-[#35445A] font-caveat text-3xl relative left-10">
+            &#60;/h2&#62;
+          </span>
+        </div>
+
+        <div className="leading-none w-fit flex flex-col ml-10">
+          <span className="align-end text-[#35445A] font-caveat text-3xl">
+            &#60;h3&#62;
+          </span>
+          <h1 className="text-[3rem] text-[#7C86A3] ml-10">
+            THINGS FOR THE WEB.
+          </h1>
+          <span className="self-end text-[#35445A] font-caveat text-3xl relative left-10">
+            &#60;/h3&#62;
+          </span>
+        </div>
+        <span className="align-end text-[#35445A] font-caveat text-3xl relative">
+          &#60;/div&#62;
+        </span>
+      </div>
+    </main>
+  );
+}
