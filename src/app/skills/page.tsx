70b3be4ceb18350b8b74a11d1395efae75258693
sahadat-sk <mss.21U10442@btech.nitdgp.ac.in>
2023-06-05 12:08:19 +0530

    [FIX] overflow in skills page
    The text Photoshop & Illustrator was overflowing from the center box in skills page, changed the height of the center box to min content for medium screens

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -1,14 +1,14 @@
-import TypedText from "../../../components/typed";
-import "animate.css";
-import "./page.css";
+import TypedText from '../../../components/typed';
+import 'animate.css';
+import './page.css';
 
 const Home = () => {
-  const strings = "sudo echo $THINGS_I_KNOW";
+  const strings = 'sudo echo $THINGS_I_KNOW';
   return (
-    <main className="h-screen w-full font-fira flex justify-center items-center">
-      <div className=" md:w-[780px] md:h-[450px] w-full h-full md:bg-[rgb(20,27,49,0.7)] bg-[rgb(20,27,49,0.5)] rounded-lg border border-[#2E2F30]">
+    <main className="flex items-center justify-center w-full h-screen font-fira">
+      <div className=" md:w-[780px] md:h-min w-full h-full md:bg-[rgb(20,27,49,0.7)] bg-[rgb(20,27,49,0.5)] rounded-lg border border-[#2E2F30]">
         <div className="rounded-t-lg  w-full h-12 md:h-10 bg-[#06080F] flex items-center">
-          <div className="ml-2 hidden md:flex">
+          <div className="hidden ml-2 md:flex">
             <div className="h-3 w-3 md:h-5 md:w-5 mx-1 rounded-full bg-[#9207FF]"></div>
             <div className="h-3 w-3 md:h-5 md:w-5 mx-1 rounded-full bg-[#B8B6B6]"></div>
             <div className="h-3 w-3 md:h-5 md:w-5 mx-1 rounded-full bg-[#61D02D]"></div>
@@ -19,8 +19,8 @@ const Home = () => {
         </div>
         <div className="m-5 md:m-7 text-lg md:text-2xl text-[#B8B6B6]">
           <h1>
-            <span className="text-[#61D02D]">a!man@portfolio:~$ </span>{" "}
-            <TypedText typeSpeed={50} strings={strings} showCursor={true} />{" "}
+            <span className="text-[#61D02D]">a!man@portfolio:~$ </span>{' '}
+            <TypedText typeSpeed={50} strings={strings} showCursor={true} />{' '}
           </h1>
           <div className="mt-5 animate__fadeIn animate__animated animate__delay-2s animate__fast">
             <ul>
